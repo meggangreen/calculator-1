856a03e5a51b37754f9c99577b2b04325f9c3a17
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 12:53:59 -0700

    calcutor with added functions, add_mult and add_cubes

--- arithmetic.py
+++ arithmetic.py
@@ -42,3 +42,13 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Return sum of first 2 nums * third"""
+
+    return (num1 + num2) * num3
+
+def add_cubes(num1,num2):
+    """Return sum of two cubes"""
+
+    return num1 ** 3 + num2 ** 3
